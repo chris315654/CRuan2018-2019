c9f93d5cbe349e27dd12d43cf2874dd75442a750
APCS3rd <APCS3rd@MHS-Math2-19>
2018-08-20 10:49:30 -0700

    My sec commit

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -10,6 +10,6 @@ public class Hello_world
 	public static void main(String[] args)
     {
         System.out.println("Hello World! From Chris");
-        System.out.println("Yeeeeeeeeeeeeeeeeeeeeet");
+        System.out.println("Yooooooooooo");
     }
 }
